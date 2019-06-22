638a45330c28414c12f649a64a351b4a9ba2a6c9
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-06-22 15:51:35 +1000

    Render: support unicode paths

--- Render/framework.h
+++ Render/framework.h
@@ -13,34 +13,34 @@ DWORD* resolutionType = (DWORD*)0x140EDA5D4;
 
 using namespace std;
 
-string DirPath() {
-	char buffer[MAX_PATH];
-	GetModuleFileName(NULL, buffer, MAX_PATH);
-	string::size_type pos = string(buffer).find_last_of("\\/");
-	return string(buffer).substr(0, pos);
+wstring DirPath() {
+	WCHAR buffer[MAX_PATH];
+	GetModuleFileNameW(NULL, buffer, MAX_PATH);
+	wstring::size_type pos = wstring(buffer).find_last_of(L"\\/");
+	return wstring(buffer).substr(0, pos);
 }
 
-string CONFIG_FILE_STRING = DirPath() + "\\plugins\\config.ini";
-LPCSTR CONFIG_FILE = CONFIG_FILE_STRING.c_str();
+wstring CONFIG_FILE_STRING = DirPath() + L"\\plugins\\config.ini";
+LPCWSTR CONFIG_FILE = CONFIG_FILE_STRING.c_str();
 
-int nFullscreen = GetPrivateProfileInt("resolution", "fullscreen", TRUE, CONFIG_FILE);
-int nBorderless = GetPrivateProfileInt("resolution", "borderless", FALSE, CONFIG_FILE);
-int nWidth = GetPrivateProfileInt("resolution", "width", 1280, CONFIG_FILE);
-int nHeight = GetPrivateProfileInt("resolution", "height", 720, CONFIG_FILE);
-int nRefreshRate = GetPrivateProfileInt("resolution", "refreshrate", 60, CONFIG_FILE);
-int nBitDepth = GetPrivateProfileInt("resolution", "bitdepth", 32, CONFIG_FILE);
+int nFullscreen = GetPrivateProfileIntW(L"resolution", L"fullscreen", TRUE, CONFIG_FILE);
+int nBorderless = GetPrivateProfileIntW(L"resolution", L"borderless", FALSE, CONFIG_FILE);
+int nWidth = GetPrivateProfileIntW(L"resolution", L"width", 1280, CONFIG_FILE);
+int nHeight = GetPrivateProfileIntW(L"resolution", L"height", 720, CONFIG_FILE);
+int nRefreshRate = GetPrivateProfileIntW(L"resolution", L"refreshrate", 60, CONFIG_FILE);
+int nBitDepth = GetPrivateProfileIntW(L"resolution", L"bitdepth", 32, CONFIG_FILE);
 
-int nIntRes = GetPrivateProfileInt("resolution", "r.enable", FALSE, CONFIG_FILE);
-int nIntResWidth = GetPrivateProfileInt("resolution", "r.width", 1280, CONFIG_FILE);
-int nIntResHeight = GetPrivateProfileInt("resolution", "r.height", 720, CONFIG_FILE);
+int nIntRes = GetPrivateProfileIntW(L"resolution", L"r.enable", FALSE, CONFIG_FILE);
+int nIntResWidth = GetPrivateProfileIntW(L"resolution", L"r.width", 1280, CONFIG_FILE);
+int nIntResHeight = GetPrivateProfileIntW(L"resolution", L"r.height", 720, CONFIG_FILE);
 
-int nStereo = GetPrivateProfileInt("patches", "stereo", FALSE, CONFIG_FILE);
-int nCursor = GetPrivateProfileInt("patches", "cursor", TRUE, CONFIG_FILE);
-int nTAA = GetPrivateProfileInt("graphics", "TAA", TRUE, CONFIG_FILE);
-int nMLAA = GetPrivateProfileInt("graphics", "MLAA", TRUE, CONFIG_FILE);
+int nStereo = GetPrivateProfileIntW(L"patches", L"stereo", FALSE, CONFIG_FILE);
+int nCursor = GetPrivateProfileIntW(L"patches", L"cursor", TRUE, CONFIG_FILE);
+int nTAA = GetPrivateProfileIntW(L"graphics", L"TAA", TRUE, CONFIG_FILE);
+int nMLAA = GetPrivateProfileIntW(L"graphics", L"MLAA", TRUE, CONFIG_FILE);
 
-int nFPSLimit = GetPrivateProfileInt("graphics", "FPS.Limit", 0, CONFIG_FILE);
+int nFPSLimit = GetPrivateProfileIntW(L"graphics", L"FPS.Limit", 0, CONFIG_FILE);
 
-//auto nBitDepth = GetPrivateProfileIntW(L"resolution", L"bitdepth", 32, CONFIG_FILE);
-//auto nRefreshRate = GetPrivateProfileIntW(L"resolution", L"refreshrate", 60, CONFIG_FILE);
+//auto nBitDepth = GetPrivateProfileIntW(L"resolutionL", L"bitdepth", 32, CONFIG_FILE);
+//auto nRefreshRate = GetPrivateProfileIntW(L"resolutionL", L"refreshrate", 60, CONFIG_FILE);
 
